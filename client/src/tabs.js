20d1282daa34d8f8d382c641c292e06b0903c11b
Daeun Jung <daeun.jung@ruhr-uni-bochum.de>
2024-10-29 22:22:55 +0100

    params for invite code

--- client/src/tabs.js
+++ client/src/tabs.js
@@ -9,3 +9,16 @@ currentTab.subscribe((value) => {
 });
 
 globalThis.currentTab = currentTab;
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+export let paramsInviteCode = params.inviteCode;
+
+if (paramsInviteCode) {
+    currentTab.set('community');
+}
+
+export function clearParamsInviteCode() {
+    paramsInviteCode = null;
+    history.replaceState({}, '', window.location.href.split('/?')[0]);
+}
