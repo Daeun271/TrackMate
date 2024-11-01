7ea916409b380cbaef7b246cbc5c9fd2087d542e
Daeun Jung <daeun.jung@ruhr-uni-bochum.de>
2024-11-01 19:58:58 +0100

    frontend for validating and updating password

--- client/src/settings/PasswordModal.svelte
+++ client/src/settings/PasswordModal.svelte
@@ -2,36 +2,155 @@
     import Modal from '../components/Modal.svelte';
     import Button from '../components/Button.svelte';
     import Loader from '../components/Loader.svelte';
+    import { validatePassword, updatePassword } from './user-field-validator';
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
 
     let password = '';
     let isLoading = false;
     let errorMessage = '';
+    let shouldBeValidated = true;
+    let newPassword = '';
+    let isLoading2 = false;
+    let errorMessage2 = '';
 
     export let isOpen = false;
+
+    async function validateUserEmail() {
+        if (isLoading) {
+            return;
+        }
+        isLoading = true;
+
+        if (password.trim() === '') {
+            errorMessage = 'Password cannot be empty';
+            isLoading = false;
+            shouldBeValidated = true;
+            return;
+        }
+
+        try {
+            const isValidPassword = await validatePassword(password);
+
+            if (!isValidPassword) {
+                errorMessage = 'An error occurred. Please try again';
+                isLoading = false;
+                shouldBeValidated = true;
+                return;
+            }
+        } catch (error) {
+            if (error.message === 'invalid password') {
+                errorMessage = 'Please enter a valid password';
+            } else if (error.message === 'wrong password') {
+                errorMessage = 'Wrong password. Please try again';
+            } else {
+                errorMessage = 'An error occurred. Please try again';
+            }
+            isLoading = false;
+            shouldBeValidated = true;
+            return;
+        }
+
+        isLoading = false;
+        errorMessage = '';
+        shouldBeValidated = false;
+    }
+
+    async function updateUserPassword() {
+        if (isLoading2) {
+            return;
+        }
+        isLoading2 = true;
+
+        if (newPassword.trim() === '') {
+            errorMessage2 = 'Password cannot be empty';
+            isLoading2 = false;
+            return;
+        }
+
+        try {
+            await updatePassword(newPassword);
+        } catch (error) {
+            if (error.message === 'invalid password') {
+                errorMessage2 = 'Please enter a valid password';
+            } else if (error.message === 'same password') {
+                errorMessage2 = 'Please enter a different password';
+            } else {
+                errorMessage2 = 'An error occurred. Please try again';
+            }
+            isLoading2 = false;
+            return;
+        }
+
+        isOpen = false;
+        isLoading2 = false;
+        shouldBeValidated = true;
+        errorMessage2 = '';
+        newPassword = '';
+        dispatch('success', { message: 'Password updated successfully' });
+    }
 </script>
 
-<Modal bind:isOpen isPopup={true}>
+<Modal
+    bind:isOpen
+    isPopup={true}
+    on:close={() => {
+        isLoading = false;
+        password = '';
+        errorMessage = '';
+        shouldBeValidated = true;
+    }}
+>
     <div class="modal-wrapper">
         <div class="input-container">
-            <label for="password"
-                >New Password<span class="required">*</span></label
-            >
-            <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter your new password"
-                bind:value={password}
-            />
-
-            <Button bind:isLoading isExpanded={true}>
-                {#if isLoading}
-                    <Loader></Loader>
-                {:else}
-                    <span>Change</span>
-                {/if}
-            </Button>
-            <p class="error-message">{errorMessage}</p>
+            {#if shouldBeValidated}
+                <label for="password"
+                    >Current Password<span class="required">*</span></label
+                >
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your current password to continue"
+                    bind:value={password}
+                />
+                <Button
+                    bind:isLoading
+                    isExpanded={true}
+                    on:click={validateUserEmail}
+                >
+                    {#if isLoading}
+                        <Loader></Loader>
+                    {:else}
+                        <span>Continue</span>
+                    {/if}
+                </Button>
+                <p class="error-message">{errorMessage}</p>
+            {:else}
+                <label for="new-password"
+                    >New Password<span class="required">*</span></label
+                >
+                <input
+                    type="password"
+                    id="new-password"
+                    name="new-password"
+                    placeholder="Enter your new password"
+                    bind:value={newPassword}
+                />
+                <Button
+                    bind:isLoading={isLoading2}
+                    isExpanded={true}
+                    on:click={updateUserPassword}
+                >
+                    {#if isLoading2}
+                        <Loader></Loader>
+                    {:else}
+                        <span>Change</span>
+                    {/if}
+                </Button>
+                <p class="error-message">{errorMessage2}</p>
+            {/if}
         </div>
     </div>
 </Modal>
---
+++ client/src/settings/user-field-validator.js
@@ -0,0 +1,53 @@
+import {
+    updateUserEmail,
+    validateUserPassword,
+    updateUserPassword,
+} from '../api';
+
+export async function updateEmail(newEmail) {
+    try {
+        const res = await updateUserEmail(newEmail);
+        return res.email;
+    } catch (e) {
+        if (e.detail === 'empty email') {
+            throw new Error('Please enter an email address');
+        } else if (e.detail === 'invalid email') {
+            throw new Error('Please enter a valid email address');
+        } else if (e.detail === 'email already in use') {
+            throw new Error('This email address is invalid');
+        }
+    }
+}
+
+export async function validatePassword(password) {
+    try {
+        const res = await validateUserPassword(password);
+        return res;
+    } catch (e) {
+        if (
+            e.detail === 'Password must be at least 8 characters' ||
+            e.detail === 'Password must have at least one numeral' ||
+            e.detail === 'Password must have at least one letter'
+        ) {
+            throw new Error('invalid password');
+        } else if (e.detail === 'wrong password') {
+            throw new Error('wrong password');
+        }
+    }
+}
+
+export async function updatePassword(newPassword) {
+    try {
+        await updateUserPassword(newPassword);
+    } catch (e) {
+        if (
+            e.detail === 'Password must be at least 8 characters' ||
+            e.detail === 'Password must have at least one numeral' ||
+            e.detail === 'Password must have at least one letter'
+        ) {
+            throw new Error('invalid password');
+        } else if (e.detail === 'same password') {
+            throw new Error('same password');
+        }
+    }
+}
